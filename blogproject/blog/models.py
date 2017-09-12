6c6350fb85b8bb71d38135b1680308842c1dacd0
zero <547644673@qq.com>
2017-09-12 22:10:42 +0800

    test

---
+++ blogproject/blog/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.urls import reverse
+# User是一个自带的模型类，里面是用户的字段
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=64)
+    body = models.TextField()
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    # 摘要
+    excerpt = models.CharField(max_length=256, blank=True)
+    # 关系
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)
+    author = models.ForeignKey(User)
+    # 文章阅读量
+    views = models.PositiveIntegerField(default=0)
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+
+    # 看不到的属性：comment_set
+    class Meta:
+        ordering = ['-created_time', '-modified_time']
+
+    def get_absolute_url(self):
+        # 使用reverse函数，生成一个url，例如 post / 1
+        return reverse('blog:detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.title
---
+++ blogproject/blog/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
+from markdown import markdown
+from comments.forms import CommentForm
+from django.http import HttpResponse
+
+
+def index(request):
+    post_list = Post.objects.all()
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+    # return HttpResponse('<h1>hello world</h1>')
+
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.increase_views()
+    post.body = markdown(post.body, extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        'markdown.extensions.toc',
+    ])
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+    context = {
+        'post': post,
+        'form': form,
+        'comment_list': comment_list,
+    }
+    return render(request, 'blog/detail.html', context=context)
+
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
